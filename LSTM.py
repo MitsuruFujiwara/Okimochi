df489ff1f0ab1f8644102bf56ac526fdd7d4aa5c
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-01-23 22:34:27 +0900

    Fix model

--- LSTM.py
+++ LSTM.py
@@ -29,25 +29,40 @@ def main():
 
     # Load Dataset
     conn = sqlite3.connect("data.db")
+    conn_val = sqlite3.connect('data20180112.db')
+
     df = pd.read_sql('select * from data', conn)
+    df_val = pd.read_sql('select * from data', conn_val)
+
+    # drop score = 0 data
+    df = df[df['Label'] != '□']
+    df_val = df_val[df_val['Label'] != '□']
 
     # Convert label to score
-    score = {'○':1, '□':0, '▲':-1, '×':-2, '◎':2}
+    score = {'○':1,'▲':0, '×':0, '◎':1}
 
     # Get tokenized text list
     print("tokenizing texts...")
     tokenized_text_list = [tokenize(texts) for texts in df.Comment]
+    tokenized_text_list_val = [tokenize(texts) for texts in df_val.Comment]
+
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(tokenized_text_list)
     seq = tokenizer.texts_to_sequences(tokenized_text_list)
+    seq_val = tokenizer.texts_to_sequences(tokenized_text_list_val)
 
     # Define training data
-    Y = np.array(df.Label.map(score).values).reshape((Y.shape[0],1))
+    Y = np.array(df.Label.map(score).values)
+    Y = np.reshape(Y, (Y.shape[0],1))
+    Y_val = np.array(df_val.Label.map(score).values)
+    Y_val = np.reshape(Y_val, (Y_val.shape[0],1))
+
     X = sequence.pad_sequences(seq, maxlen=400)
+    X_val = sequence.pad_sequences(seq_val, maxlen=400)
 
     # model parameters
     in_out_neurons = 1
-    hidden_neurons = 250
+    hidden_neurons = 200
 
     # Define LSTM model
     model = Sequential()
@@ -59,19 +74,37 @@ def main():
                    dropout_U=0.3,
                    dropout_W=0.3))
     model.add(Dense(in_out_neurons))
-    model.add(Activation("linear"))
+    model.add(Activation('tanh'))
 
     # Compile model
-    model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+    history = model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # fit model
-    model.fit(X, Y, nb_epoch=50)
+    model.fit(X, Y, nb_epoch=1, validation_data=(X_val, Y_val))
 
     # save model
     model.save('LSTM.h5')
 
     # Close Database
     conn.close()
+
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
 if __name__ == '__main__':
     main()
